50bd8a242c2f44050b4f97dd1ff7a4b0b5eb67db
masedi54 <aslekmak54@gmail.com>
2023-08-02 14:54:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+const container = document.querySelector('.container');
+let deg = 0;
+btn.onclick = () => {
+    deg += 90;
+    container.style.transform = 'rotate(' + deg + 'deg)';
+};
